0da2069a18fc619de00464d2ee8d6fa42e7f273f
TzuYao <chaotzuyao@gmail.com>
2022-09-06 22:39:01 +0800

    Test Calendar

--- src/main/java/com/peterchao/SpringBootQRCode/SpringBootQrCodeApplication.java
+++ src/main/java/com/peterchao/SpringBootQRCode/SpringBootQrCodeApplication.java
@@ -12,19 +12,26 @@ public class SpringBootQrCodeApplication {
 		SpringApplication.run(SpringBootQrCodeApplication.class, args);
 		Calendar startTime = Calendar.getInstance();
 		startTime.set(Calendar.YEAR,2022);
-		startTime.set(Calendar.MONTH,9);
+		startTime.set(Calendar.MONTH,10);
 		startTime.set(Calendar.DAY_OF_MONTH,4);
-		startTime.add(Calendar.MONTH,-1);
 		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
 		String startCurrentTime = format.format(startTime.getTime());
 		System.out.println(startCurrentTime);
 
 		Calendar lastTime = Calendar.getInstance();
 		lastTime.set(Calendar.YEAR,2022);
-		lastTime.set(Calendar.MONTH,9);
-		lastTime.set(Calendar.DAY_OF_MONTH,1);
-		lastTime.add(Calendar.MONTH,-1);
-		String lastCurrentTime = format.format(startTime.getTime());
+		lastTime.set(Calendar.MONTH,10);
+		lastTime.set(Calendar.DAY_OF_MONTH,30);
+		String lastCurrentTime = format.format(lastTime.getTime());
 		System.out.println("Last" + lastCurrentTime);
+		if (startCurrentTime.substring(4,5).equals(lastCurrentTime.substring(4,5))){
+			System.out.println("same");
+			System.out.println(startCurrentTime.substring(4,6));
+			System.out.println(lastCurrentTime.substring(4,6));
+		}else {
+			System.out.println("diff");
+			System.out.println(startCurrentTime.substring(4,6));
+			System.out.println(lastCurrentTime.substring(4,6));
+		}
 	}
 }
